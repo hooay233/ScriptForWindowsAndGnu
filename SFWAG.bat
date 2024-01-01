49483bebed2ff37c485ea14ec67543da8a3dc7eb
hooay233 <hoorayof_hooay@163.com>
2024-01-01 15:18:47 +0800

    解决了乱码和换行符的问题
    - 通过将中文改为英文解决了乱码
    - 将Linux部分的换行符改为 unix 换行符

--- SFWAG.bat
+++ SFWAG.bat
@@ -3,61 +3,61 @@
 @echo OFF
 goto :winpart
 
-echo "此脚本既可在Windows运行，也能在GNU/Linux上运行"
-echo "当前在GNU/Linux上运行"
-echo "输入你要运行的测试项目（1或2或3）："
-echo "1: 装逼"
-echo "2: 两数相加"
-echo "3: 猜数字"
+clear
+echo "This Script Can Run on Both of Windows and GNU"
+echo "it's run on GNU/Linux now"
+echo "select your test project(1 or 2 or 3)"
+echo "1: show files"
+echo "2: add two numbers"
+echo "3: guess number"
 read -a project
 if [ $project == 1 ]
 then
 	ls -Rla ~
 elif [ $project == 2 ]
 then
-	echo "输入第一个数："
+	echo "input first number:"
 	read -a a
-	echo "输入第二个数："
+	echo "input sencond number"
 	read -a b
 	n=`expr $a + $b`
-	echo "他俩的和是 $n "
+	echo "the sum of them is $n "
 elif [ $project == 3 ]
 then
 	r=`expr $RANDOM`
 	n=`echo "$r / 327.67" | bc`
-	echo "猜一个 0 到 100 的数"
+	echo "guess a number from 0 to 100"
 	for i in {4..0}
 	do
-		echo -n "我猜这个数是："
+		echo -n "I guess this number is: "
 		read -a a
 		if [ $a -eq $n ]
 		then
-			echo "你猜对了！"
+			echo "bingo!"
 			exit 0
 		elif [ $a -lt $n ]
 		then
-			echo "你猜小了！"
+			echo "too small"
 		elif [ $a -gt $n ]
 		then
-			echo "你猜大了！"
+			echo "too big"
 		fi
-		echo "你还有 $i 次机会"
+		echo "you had $i shot"
 	done
-	echo "这个数是 $n ！这都猜不中你个傻逼！"
+	echo "fool! this number is $n !"
 fi
 
 
 exit 0
 
 :winpart
-
-echo 此脚本既可在Windows运行，也能在GNU/Linux上运行
-echo 当前在Windows上运行
-echo 输入你要运行的测试项目（实际上只能选1，其他的还没做）：
-echo （如果想看其他测试项目，请用linux运行此脚本）
-echo 1: 装逼
-echo 2: 两数相加
-echo 3: 猜数字
+echo This Script Can Run on Both of Windows and GNU
+echo it's run on Windows now
+echo select your test project
+echo (you have to select 1, because 2 and 3 is not finish now)
+echo 1: show files
+echo 2: add two numbers
+echo 3: guess number
 set /p project=""
 dir /s C:\
 
